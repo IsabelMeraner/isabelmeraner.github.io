d6aee052ca451776713a58e0d670ed986eec0b67
IsabelMeraner <isabel.meraner@uzh.ch>
2025-12-02 10:45:17 +0100

    add effect.

--- js/script.js
+++ js/script.js
@@ -26,3 +26,53 @@ function carousel() {
     slides[slideIndex - 1].classList.add("active");
     setTimeout(carousel, 2000);
 }
+
+const gallery = document.getElementById("gallery");
+const splashContainer = document.getElementById("splash-container");
+
+gallery.addEventListener("click", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    // Background flash
+    const flash = document.createElement("div");
+    flash.className = "magic-flash";
+    document.body.appendChild(flash);
+
+    setTimeout(()=> flash.remove(), 1200);
+
+    // Spawn particles
+    for (let i = 0; i < 90; i++) {
+        spawnParticle(x, y);
+    }
+});
+
+function spawnParticle(x, y) {
+    const p = document.createElement("div");
+
+    // Star or circle
+    p.className = "particle" + (Math.random() > 0.7 ? " star" : "");
+
+    const size = Math.random() * 10 + 4;
+    p.style.width = size + "px";
+    p.style.height = size + "px";
+
+    p.style.left = x + "px";
+    p.style.top = y + "px";
+
+    // Random blast vector
+    const angle = Math.random() * Math.PI * 2;
+    const distance = Math.random() * 350 + 100;
+
+    p.style.setProperty("--x", Math.cos(angle) * distance);
+    p.style.setProperty("--y", Math.sin(angle) * distance);
+
+    splashContainer.appendChild(p);
+
+    // Auto cleanup
+    setTimeout(() => {
+        p.remove();
+    }, 1400);
+}
+
+document.getElementById("year").textContent = new Date().getFullYear();
